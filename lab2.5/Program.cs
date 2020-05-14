83f7b6018e36d80ebfbcb3010e983a3245785836
Dmytro14 <60977979+Dmytro14@users.noreply.github.com>
2020-05-15 00:29:15 +0300

    first test

---
+++ lab2.5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace lab2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y: ");
+            int y = int.Parse(Console.ReadLine());
+            int[,] mas = new int[x, y];
+            Console.WriteLine();
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write("mas[" + i + "," + j + "]: ");
+                    mas[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (i == j)
+                    {
+                        mas[i, j] = 0;
+
+                    }
+                    else
+                    {
+                        mas[i, j] = mas[i, j]* mas[i, j];
+                    }
+                    Console.Write(" mas[" + i + "," + j + "]: " + mas[i, j] + "\t");
+                }
+
+                Console.WriteLine();
+            }
+           
+
+        }
+    }
+}
